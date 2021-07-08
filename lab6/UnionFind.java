6dbeda5c3482311c985df4ca8a62f26134249428
XmchxCoder <1394466835@qq.com>
2021-07-08 17:04:23 +0800

    完成lab6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,51 +1,79 @@
+import java.util.Arrays;
+
 public class UnionFind {
 
-    // TODO - Add instance variables?
+	// TODO - Add instance variables?
+	private final int[] parents;
+	private final int[] sizes;
 
-    /* Creates a UnionFind data structure holding n vertices. Initially, all
-       vertices are in disjoint sets. */
-    public UnionFind(int n) {
-        // TODO
-    }
+	/* Creates a UnionFind data structure holding n vertices. Initially, all
+	   vertices are in disjoint sets. */
+	public UnionFind(int n) {
+		this.parents = new int[n];
+		this.sizes = new int[n];
+		Arrays.fill(parents, -1);
+		Arrays.fill(sizes, 1);
+	}
 
-    /* Throws an exception if v1 is not a valid index. */
-    private void validate(int vertex) {
-        // TODO
-    }
+	/* Throws an exception if v1 is not a valid index. */
+	private void validate(int vertex) {
+		if (vertex < 0 || vertex >= parents.length) throw new IndexOutOfBoundsException();
+	}
 
-    /* Returns the size of the set v1 belongs to. */
-    public int sizeOf(int v1) {
-        // TODO
-        return -1;
-    }
+	/* Returns the size of the set v1 belongs to. */
+	public int sizeOf(int v1) {
+		int root = find(v1);
+		return sizes[root];
+	}
 
-    /* Returns the parent of v1. If v1 is the root of a tree, returns the
-       negative size of the tree for which v1 is the root. */
-    public int parent(int v1) {
-        // TODO
-        return -1;
-    }
+	/* Returns the parent of v1. If v1 is the root of a tree, returns the
+	   negative size of the tree for which v1 is the root. */
+	public int parent(int v1) {
+		validate(v1);
+		return parents[v1] < 0 ? -sizes[v1] : parents[v1];
+	}
 
-    /* Returns true if nodes v1 and v2 are connected. */
-    public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
-    }
+	/* Returns true if nodes v1 and v2 are connected. */
+	public boolean connected(int v1, int v2) {
+		return find(v1) == find(v2);
+	}
 
-    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
-       elements, and a union-by-size heuristic is used. If the sizes of the sets
-       are equal, tie break by connecting v1's root to v2's root. Unioning a 
-       vertex with itself or vertices that are already connected should not 
-       change the sets but may alter the internal structure of the data. */
-    public void union(int v1, int v2) {
-        // TODO
-    }
+	/* Connects two elements v1 and v2 together. v1 and v2 can be any valid
+	   elements, and a union-by-size heuristic is used. If the sizes of the sets
+	   are equal, tie break by connecting v1's root to v2's root. Unioning a
+	   vertex with itself or vertices that are already connected should not
+	   change the sets but may alter the internal structure of the data. */
+	public void union(int v1, int v2) {
+		int pV1 = find(v1);
+		int pV2 = find(v2);
 
-    /* Returns the root of the set V belongs to. Path-compression is employed
-       allowing for fast search-time. */
-    public int find(int vertex) {
-        // TODO
-        return -1;
-    }
+		int szV1 = sizeOf(pV1);
+		int szV2 = sizeOf(pV2);
+
+
+		if (szV1 > szV2) {
+			parents[pV2] = pV1;
+			sizes[pV1] += sizes[pV2];
+			if (parents[v1] != -1) { // not root
+				sizes[v1] += sizes[pV2];
+			}
+		} else {
+			parents[pV1] = pV2;
+			sizes[pV2] += sizes[pV1];
+			if (parents[v2] != -1) { // not root
+				sizes[v2] += sizes[pV1];
+			}
+		}
+	}
+
+	/* Returns the root of the set V belongs to. Path-compression is employed
+	   allowing for fast search-time. */
+	public int find(int vertex) {
+		validate(vertex);
+		while (parents[vertex] != -1) {
+			vertex = parents[vertex];
+		}
+		return vertex;
+	}
 
 }
---
+++ lab6/UnionFindTest.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author xmchx (sunhuayangak47@gmail.com)
+ */
+public class UnionFindTest {
+	@Test
+	public void test() {
+		UnionFind uf = new UnionFind(10);
+		uf.union(1, 0);
+		uf.union(0, 2);
+		uf.union(0, 3);
+		uf.union(0, 4);
+		uf.union(0, 5);
+
+		assertTrue(uf.connected(0, 5));
+		assertTrue(uf.connected(2, 5));
+		assertTrue(uf.connected(3, 5));
+		assertTrue(uf.connected(1, 4));
+
+		assertEquals(6, uf.sizeOf(0));
+		assertEquals(6, uf.sizeOf(1));
+		assertEquals(6, uf.sizeOf(2));
+
+		assertEquals(-6, uf.parent(0));
+		assertEquals(0, uf.parent(1));
+
+		uf.union(7, 6);
+		uf.union(8, 6);
+		uf.union(9, 8);
+
+		assertFalse(uf.connected(0, 6));
+		assertFalse(uf.connected(0, 9));
+
+		assertTrue(uf.connected(6, 8));
+		assertTrue(uf.connected(7, 9));
+		assertTrue(uf.connected(6, 9));
+
+		assertEquals(4, uf.sizeOf(6));
+
+		uf.union(0, 7);
+
+		assertTrue(uf.connected(1, 9));
+	}
+}
