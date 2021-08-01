b0d93fd697fdc3da96218edb5b1912f891536b24
XmchxCoder <1394466835@qq.com>
2021-08-01 14:41:21 +0800

    完成 Challenge Lab8

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,27 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+	private int maxPassengerCount;
 
-    public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
-    }
+	public FlightSolver(ArrayList<Flight> flights) {
+		PriorityQueue<Flight> startPQ = new PriorityQueue<>(Comparator.comparingInt(Flight::startTime));
+		PriorityQueue<Flight> endPQ = new PriorityQueue<>(Comparator.comparingInt(Flight::endTime));
+		startPQ.addAll(flights);
+		endPQ.addAll(flights);
 
-    public int solve() {
-        /* FIX ME */
-        return -1;
-    }
+		int tally = 0;
+		while (!startPQ.isEmpty()) {
+			if (startPQ.peek().startTime() <= endPQ.peek().endTime()) {
+				tally += startPQ.poll().passengers();
+				maxPassengerCount = Math.max(tally, maxPassengerCount);
+			} else {
+				tally -= endPQ.poll().passengers();
+			}
+		}
+	}
+
+	public int solve() {
+		return maxPassengerCount;
+	}
 
 }
--- clab8/RabinKarpAlgorithm.java
+++ clab8/RabinKarpAlgorithm.java
@@ -1,12 +1,34 @@
 public class RabinKarpAlgorithm {
 
 
-    /**
-     * This algorithm returns the starting index of the matching substring.
-     * This method will return -1 if no matching substring is found, or if the input is invalid.
-     */
-    public static int rabinKarp(String input, String pattern) {
-        return -1;
-    }
+	/**
+	 * This algorithm returns the starting index of the matching substring.
+	 * This method will return -1 if no matching substring is found, or if the input is invalid.
+	 */
+	public static int rabinKarp(String input, String pattern) {
+		int n = input.length();
+		int m = pattern.length();
+		if (n < m) {
+			return -1;
+		}
+
+		RollingString rs1 = new RollingString(input.substring(0, m), m);
+		RollingString rs2 = new RollingString(pattern, m);
+
+		int hash1 = rs1.hashCode();
+		int hash2 = rs2.hashCode();
+		for (int i = 0; i < n - m + 1; i++) {
+			if (hash1 == hash2) {
+				if (rs1.equals(rs2)) {
+					return i;
+				}
+			}
+			if (i < n - m) {
+				rs1.addChar(input.charAt(i + m));
+				hash1 = rs1.hashCode();
+			}
+		}
+		return -1;
+	}
 
 }
