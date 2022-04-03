e67961e87792200aa9e0c19c5856fabbb5e7fa97
XmchxCoder <1394466835@qq.com>
2022-04-03 08:45:55 +0800

    proj2c: 完成part2

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -2,6 +2,7 @@ package bearmaps.proj2c;
 
 import bearmaps.hw4.streetmap.Node;
 import bearmaps.hw4.streetmap.StreetMapGraph;
+import bearmaps.proj2ab.KdTree;
 import bearmaps.proj2ab.Point;
 
 import java.util.*;
@@ -10,33 +11,53 @@ import java.util.*;
  * An augmented graph that is more powerful that a standard StreetMapGraph.
  * Specifically, it supports the following additional operations:
  *
- *
  * @author Alan Yao, Josh Hug, ________
  */
 public class AugmentedStreetMapGraph extends StreetMapGraph {
+    private KdTree tree;
+    private Map<Point, Long> pointIdMap = new HashMap<>();
 
     public AugmentedStreetMapGraph(String dbPath) {
         super(dbPath);
         // You might find it helpful to uncomment the line below:
-        // List<Node> nodes = this.getNodes();
+        List<Node> nodes = this.getNodes();
+        List<Point> points = new ArrayList<>();
+
+        nodes.forEach(node -> {
+            if (hasNeighbours(node)) {
+                Point point = new Point(node.lon(), node.lat());
+                pointIdMap.put(point, node.id());
+                points.add(point);
+            }
+        });
+
+
+        tree = new KdTree(points);
+    }
+
+    private boolean hasNeighbours(Node node) {
+        return this.neighbors(node.id()).size() > 0;
     }
 
 
     /**
      * For Project Part II
      * Returns the vertex closest to the given longitude and latitude.
+     *
      * @param lon The target longitude.
      * @param lat The target latitude.
      * @return The id of the node in the graph closest to the target.
      */
     public long closest(double lon, double lat) {
-        return 0;
+        Point nearest = tree.nearest(lon, lat);
+        return pointIdMap.get(nearest);
     }
 
 
     /**
      * For Project Part III (gold points)
      * In linear time, collect all the names of OSM locations that prefix-match the query string.
+     *
      * @param prefix Prefix string to be searched for. Could be any case, with our without
      *               punctuation.
      * @return A <code>List</code> of the full names of locations whose cleaned name matches the
@@ -50,6 +71,7 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * For Project Part III (gold points)
      * Collect all locations that match a cleaned <code>locationName</code>, and return
      * information about each node that matches.
+     *
      * @param locationName A full name of a location searched for.
      * @return A list of locations whose cleaned name matches the
      * cleaned <code>locationName</code>, and each location is a map of parameters for the Json
@@ -67,6 +89,7 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     /**
      * Useful for Part III. Do not modify.
      * Helper to process strings into their "cleaned" form, ignoring punctuation and capitalization.
+     *
      * @param s Input string.
      * @return Cleaned string.
      */
--- proj2c/bearmaps/test/Mytest.java
+++ proj2c/bearmaps/test/Mytest.java
@@ -1,11 +1,21 @@
 package bearmaps.test;
 
+import bearmaps.proj2c.AugmentedStreetMapGraph;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author huayang (sunhuayangak47@gmail.com)
  */
 public class Mytest {
+    @Test
+    public void testCloset() {
+        var graph = new AugmentedStreetMapGraph("../library-sp19/data/proj2c_xml/berkeley-2019.osm.xml");
+        long id = graph.closest(37.875613, -122.26009);
+        assertEquals(1281866063, id);
+    }
+
     @Test
     public void test() {
         System.out.println(Math.log(8) / Math.log(2));
